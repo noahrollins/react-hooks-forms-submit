13f40cbdb12267a4ad5a609f6cab0f4322077a8d
Noah Rollins <noahjrollins@gmail.com>
2022-11-28 12:07:29 -0600

    commit tree 673ae47e46bd27bd57be3fb485af7b48ebd471d8

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,12 +13,24 @@ function Form(props) {
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData= {
+      firstName: firstName,
+      lastName: lastName,
+    };
+    props.sendFormDataSomewhere(formData);
+    setFirstName("");
+    setLastName("");
+  }
+
 }
 
 export default Form;
